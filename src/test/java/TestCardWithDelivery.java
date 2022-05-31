304529a2e7e07bffe3cd22338a9bdcd20c934a0c
Aleksandr Shalagin <ssanchesko@gmail.com>
2022-05-31 14:50:23 +0500

    add changes + tests

--- src/test/java/TestCardWithDelivery.java
+++ src/test/java/TestCardWithDelivery.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
@@ -20,18 +22,25 @@ class TestCardWithDelivery {
         Configuration.holdBrowserOpen = true;
     }
 
+    public String generateDate (int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(4);
+
     @Test
     void shouldTestHappyPath() {
         open("http://localhost:9999/");
         $x("//input[@class='input__control']").val("Пермь");
 //        $x("//input[@type='tel']").doubleClick().sendKeys("10.06.2022");
-        $x("//input[@type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $x("//input[@type='tel']").val("10.06.2022");
-        $$("input[type='text']").get(1).setValue("Александр Холидэй");
-        $$("input[type='tel']").last().setValue("+79028383123");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val(planningDate);
+        $("[data-test-id='name'] input").val("Александр Холи-дэй");
+        $("[data-test-id='phone'] input").val("+79028383123");
         $x("//span[@class='checkbox__box']").click();
         $(withText("Забронировать")).click();
-        $x("//*[contains(text(), 'Встреча успешно забронирована на')]").should(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
+//        $x("//*[contains(text(), 'Встреча успешно забронирована на')]").should(visible, Duration.ofSeconds(15));
 
     }
 
@@ -53,12 +62,14 @@ class TestCardWithDelivery {
     void shouldTestChoosePetrozavodsk() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").val("Пе").sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER);
-        $x("//input[@type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE, "17.07.2022");
+//        $x("//input[@type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE, "17.07.2022");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.LEFT_CONTROL, "a"), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(generateDate(30));
         $$("input[type='text']").get(1).setValue("Александр Холидэй");
         $$("input[type='tel']").last().setValue("+79028383123");
         $x("//span[@class='checkbox__box']").click();
         $(withText("Забронировать")).click();
-        $x("//*[contains(text(), 'Встреча успешно забронирована на')]").should(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(30)), Duration.ofSeconds(15));
 
     }
 
@@ -66,11 +77,25 @@ class TestCardWithDelivery {
     void shouldTestDataPlusWeek() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").sendKeys("Санкт-Петербург");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER);
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE, generateDate(7));
         $("[data-test-id='name'] input").val("Александр Холидэй");
         $("[data-test-id='phone'] input").val("+79028383123");
         $("[data-test-id='agreement']").click();
         $(withText("Забронировать")).click();
-        $x("//*[contains(text(), 'Встреча успешно забронирована на')]").should(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(7)), Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestDataPlusMonthOnCalendarOn10Jule() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").sendKeys("Москва");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-step='1']").click();
+        $("[data-day='1657393200000']").click();
+        $("[data-test-id='name'] input").val("Александр Холидэй");
+        $("[data-test-id='phone'] input").val("+79028383123");
+        $("[data-test-id='agreement']").click();
+        $(withText("Забронировать")).click();
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на 10"), Duration.ofSeconds(15));
     }
 }
